977e6f81141230705b724aeecbb97b8cb1b2a818
Alvaro Felix <alvaro.felix@alumnos.udg.mx>
2021-09-05 02:02:38 -0500

    Added bicicleta and eventoSocial models

--- seguridad/main_api/models.py
+++ seguridad/main_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
 class SolicitudVideoCamaras(models.Model):
@@ -12,3 +13,30 @@ class SolicitudVideoCamaras(models.Model):
     fotoIdF = models.CharField(max_length=100)
     fotoIdB = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.codigoReporte
+    
+class Bicicleta(models.Model):
+    CodigoReporte = models.BigIntegerField()
+    CodigoAlumno = models.BigIntegerField()
+    FotoIdF = models.CharField(max_length=30)
+    FotoIdB = models.CharField(max_length=30)
+    Descripcion = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.CodigoReporte
+
+class EventoSocial(models.Model):
+    CodigoReporte = models.BigIntegerField()
+    NombreEvento = models.CharField(max_length=20)
+    FechaHora = models.DateField('date published')
+    Pronostico = models.CharField(max_length=100)
+    MensajeSeguridad = models.CharField(max_length=100)
+    Estrategia = models.CharField(max_length=100)
+    Recurso = models.CharField(max_length=100)
+    Croquis = models.CharField(max_length=100)
+    Organigrama = models.CharField(max_length=100)
+    Estado = models.CharField(max_length=100)
+    NombreEncargado = models.CharField(max_length=100)
+    TelefonoEncargado = models.BigIntegerField()
+    CorreoEncargado = models.EmailField()
