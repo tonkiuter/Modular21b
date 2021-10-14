094c6b04f7d09167f447f499c36e301164c7a868
Zagax <84048540+Zagax@users.noreply.github.com>
2021-10-14 14:28:49 -0500

    Cambios
    Zagax

--- React/seguridadreact/src/Components/RomperCandado.js
+++ React/seguridadreact/src/Components/RomperCandado.js
@@ -5,33 +5,33 @@ class RomperCandado extends Component{
     render(){
         return (
             <div>
-            <h1 class="display-3">Romper Candado</h1>
-            <form class="needs-validation">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md">
-                            Nombre de Alumno: <input class="form-control" type="text" required></input>
+            <h1 className="display-3">Romper Candado</h1>
+            <form className="needs-validation">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md">
+                            Nombre de Alumno: <input className="form-control" type="text" required></input>
                         </div>
-                        <div class="col-md">
-                            Carrera: <input class="form-control" type="text" required></input>
+                        <div className="col-md">
+                            Carrera: <input className="form-control" type="text" required></input>
                         </div>
-                        <div class="col-md">
-                            Codigo: <input class="form-control" type="text" required></input>
+                        <div className="col-md">
+                            Codigo: <input className="form-control" type="text" required></input>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md">
+                    <div className="row">
+                        <div className="col-md">
                             Fecha:
                         </div>
-                        <div class="col-md">
+                        <div className="col-md">
                             <p>
-                                Yo: <input class="form-control" type="text" id="disabledTextInput"></input>
+                                Yo: <input className="form-control" type="text" id="disabledTextInput"></input>
                                 <br></br>
                                 autorizo que el personal de seguridad interna de CUCEI, le de apetura
                                 <br></br>
                                 a mi candado que se encuentra en: 
-                                <select class="custom-select" required>
-                                    <option selected>Seleccionar...</option>
+                                <select className="custom-select" required>
+                                    <option defaultValue>Seleccionar...</option>
                                     <option value=""></option>
                                     <option value=""></option>
                                 </select>
