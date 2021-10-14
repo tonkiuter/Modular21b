75110e9d0c56cc3dff8507f7b556d27d74de4ce8
TheNachozs <the.nachozs@gmail.com>
2021-10-14 12:10:12 -0500

    a

---
+++ React/seguridadreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RomperCandado from './Components/RomperCandado';
+
+function App() {
+  return (
+    <div className="App">
+      <RomperCandado></RomperCandado>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/seguridadreact/src/Components/RomperCandado.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function RomperCandado(){
+    return (
+        <div>
+            <h1 class="display-3">Pase de Salida/Tarjeton Perdido</h1>,
+            <form class="needs-validation">
+            </form>
+        </div>
+    )
+    
+}
+
+/*const Fe = () => <h1>Hello Prro</h1>*/
+
+export default RomperCandado
