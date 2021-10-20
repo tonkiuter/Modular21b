9cc2f82fddd669839d15881cd881cd09e7cb1ca3
TheNachozs <the.nachozs@gmail.com>
2021-10-20 17:54:21 -0500

    Update HojaUrgencias.js

---
+++ React/seguridadreact/src/Components/HojaUrgencias.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class HojaUrgencias extends Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            Fecha: '',
+            Nombre: '',
+            Edad: '',
+            Adscripcion: '',
+            Codigo: '',
+            Cargo: '',
+            NoTelefono: '',
+            CUCEI: '',
+            Prepa12: '',
+            VOCA: '',
+            POLI: '',
+            CDU: '',
+            Exterior: '',
+            Ubicacion: '',
+            TrasladoCUCEI: '',
+            IMSS: '',
+            ISSSTE: '',
+            SMM: '',
+            CRM: '',
+            PRIVADO: '',
+            SeNiega: '',
+            NoAmerita: '',
+            Padecimiento: '',
+            Diabetes: '',
+            DiabetesDiagnostico: '',
+            DiabetesTratamiento: '',
+            Hipertension: '',
+            HipertensionDiagnostico: '',
+            HipertensionTratamiento: '',
+            Epilepsia: '',
+            EpilepsiaDiagnostico: '',
+            EpilepsiaTratamiento: '',
+            Asma: '',
+            AsmaDiagnostico: '',
+            AsmaTratamiento: '',
+            Cirugias: '',
+            Alergias: '',
+            Sangre: '',
+            TensionArterial: '',
+            FC: '',
+            FR: '',
+            Temperatura: '',
+            Saturacion: '',
+            Glucosa: '',
+            EscalaGlasgow: '',
+            Neurologico: '',
+            CabezaCuello: '',
+            Cardiopulmonar: '',
+            Abdomen: '',
+            Extremidades: '',
+            Diagnostico: '',
+            Condicion: '',
+            PronostioS: '',
+            Tratamiento: '',
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    submitHandler = e=>{
+        e.preventDefault()
+        console.log(this.state)
+        axios
+            .post('http://127.0.0.1:8000/hojaurgencias', this.state)
+            .then(response =>{
+                console.log(response)
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+    }
+
+    render(){
+        const{}
+        return(
+
+        )
+    }
+}
+
+export default HojaUrgencias
