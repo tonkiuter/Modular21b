dac8f608b400d017f8c1808fbde42fff0ecdfc46
TheNachozs <the.nachozs@gmail.com>
2021-11-10 01:17:32 -0600

    g

--- seguridad/seguridad/settings.py
+++ seguridad/seguridad/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangodatabase',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'genialix',
         'HOST': 'localhost',
         'PORT': '3306',
     }
